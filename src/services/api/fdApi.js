5ce40f4aee5928bf8429a143d0cfac38540eecce
zdenekpasek <zdenekpasek1@gmail.com>
2020-12-06 22:34:33 +0100

    baseURL changed to production

--- src/services/api/fdApi.js
+++ src/services/api/fdApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
-  baseURL: 'http://2b571df018da.ngrok.io',
+  baseURL: 'https://immense-escarpment-01448.herokuapp.com',
 });
 
 instance.interceptors.request.use(
